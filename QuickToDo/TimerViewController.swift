5768bad0abbd009c82590410cf71ea0d3a8b4e39
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-06-09 15:43:48 -0400

    edit timer

--- QuickToDo/TimerViewController.swift
+++ QuickToDo/TimerViewController.swift
@@ -55,11 +55,10 @@ class TimerViewController: UIViewController {
     
     func timeString(time:TimeInterval) -> String {
         
-        let hours = Int(time) / 3600
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
         
-        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+        return String(format:"%02i:%02i:%02i", minutes, seconds)
     }
    
 }
