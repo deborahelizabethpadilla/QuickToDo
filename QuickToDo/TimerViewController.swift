aa036715edad86e30e3ab79d858496a81853c5ab
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-06-09 15:15:11 -0400

    Edit timer

--- QuickToDo/TimerViewController.swift
+++ QuickToDo/TimerViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class TimerViewController: UIViewController {
     
+    //Variables
+    
+    var timer = Timer()
+    var timeLeft = 600
+    
     //Outlets
 
     @IBOutlet var timerLabel: UILabel!
@@ -24,9 +29,37 @@ class TimerViewController: UIViewController {
     //Actions
 
     @IBAction func stopButton(_ sender: Any) {
+        
+        timer.invalidate()
+        let seconds = 600
+        timerLabel.text = timeString(time: TimeInterval(seconds))
     }
     
     @IBAction func startButton(_ sender: Any) {
+        
+        timeLeft = 600
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(TimerViewController.timerRunning), userInfo: nil, repeats: true)
+    }
+    
+    func timerRunning() {
+        
+        timeLeft -= 1
+        timerLabel.text = "Time Left: \(timeLeft)"
+        
+        if timeLeft == 0 {
+            
+            timer.invalidate()
+            timerLabel.text = "Times Up!"
+        }
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
     }
    
 }
